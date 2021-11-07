526accaa0c5d1fd5f9782a5900f95b7d05502a3f
Shantanu Sharma <52274663+Carnage1x@users.noreply.github.com>
2021-11-07 17:21:16 +1100

    Create ppm.py

---
+++ ppm.py
@@ -0,0 +1,47 @@
+import requests
+import tweepy 
+import pandas as pd
+
+def ppmbot():
+    # Consumer keys and access tokens, used for Tweepy
+    CONSUMER_KEY = 'enterhere'
+    CONSUMER_SECRET = 'enterhere'
+    ACCESS_KEY = 'enterhere'
+    ACCESS_SECRET = 'enterhere'
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    
+    #Accessing the Fantasy Premier League API
+    
+    element_url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+    req = requests.get(element_url)
+    fpl_data = req.json()
+    
+    #Get the total points
+    elements_df = pd.DataFrame(fpl_data['elements'])
+    elements_df = elements_df[['web_name','now_cost','total_points']]
+    elements_df['now_cost'] = elements_df['now_cost'] / 10
+    
+    #Calculate PPM
+    elements_df['PPM'] = elements_df['total_points'] / elements_df['now_cost']
+    
+    #Get Top 5 PPM
+    ppm_df = elements_df.sort_values('PPM', ascending=False).head(5)
+    
+    #Create Your Tweet
+    ppm_list = []
+    for i,r in ppm_df.iterrows():
+        name = ppm_df.at[i,'web_name']
+        ppm_value = round(ppm_df.at[i,'PPM'],2)
+        ppm_string = name + ' -' + str(ppm_value)
+        ppm_list.append(ppm_string)
+
+    if len(ppm_list):
+        ppm_join_sting = "\n".join(ppm_list)
+        ppm_final_string = 'Top 5 PPM -\n\n' + ppm_join_sting + '\n\n#FPL #FPLCommunity'
+        api.update_status(ppm_final_string)
+
+if __name__ == '__main__':
+    ppmbot() 
